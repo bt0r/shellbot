1ad91748dca104ab90527e588c7fa27adc6110da
Thibaut <the.btor@gmail.com>
2018-03-30 21:43:26 +0200

    :hammer: Show HD pictures

--- Fun/BoobsPicture.js
+++ Fun/BoobsPicture.js
@@ -7,7 +7,7 @@ module.exports = {
         console.log('[Boobs] Fetching new booby picture')
         request(this.url, function (error, response, body) {
             let jsonResponse = JSON.parse(body);
-            let boobsPicture = "http://media.oboobs.ru/"+jsonResponse[0].preview;
+            let boobsPicture = "http://media.oboobs.ru/"+jsonResponse[0].preview.replace("boobs_preview","boobs");
             let model = jsonResponse[0].model !== null ? jsonResponse[0].model : "";
             console.log('[Boobs] New boobs found ('+message.author.username+') : ' + boobsPicture);
             message.reply(model+" - "+boobsPicture);
--- Fun/ButtsPicture.js
+++ Fun/ButtsPicture.js
@@ -7,7 +7,7 @@ module.exports = {
         console.log('[Boobs] Fetching new butts picture')
         request(this.url, function (error, response, body) {
             let jsonResponse = JSON.parse(body);
-            let buttsPicture = "http://media.obutts.ru/"+jsonResponse[0].preview;
+            let buttsPicture = "http://media.obutts.ru/"+jsonResponse[0].preview.replace("butts_preview","butts");
             let model = jsonResponse[0].model !== null ? jsonResponse[0].model : "";
             console.log('[Boobs] New butts found ('+message.author.username+') : ' + buttsPicture);
             message.reply(model+" - "+buttsPicture);
