a0cea4913781fee4de0ab2065053304765243a66
bt0r <the.btor@gmail.com>
2018-06-03 20:35:48 +0200

    :hammer: Add config Service to IOC container

---
+++ src/Service/Config.ts
@@ -0,0 +1,34 @@
+'use strict';
+import {Inject} from "typescript-ioc";
+import * as YAML from "yamljs";
+import {Channel, TextChannel} from "discord.js";
+
+
+export class Config{
+    @Inject
+    /**
+     * Config
+     * @type {Object}
+     * @private
+     */
+    private _config = YAML.load('config/config.yml');
+
+    /**
+     * Return the shellbot config
+     * @returns {Object}
+     */
+    public get config(): Object {
+        return this._config;
+    }
+
+    /**
+     * Check if a command is enabled on a a specific channel
+     * @param {string} command
+     * @param {string} channel
+     * @returns {boolean}
+     */
+    public isCommandEnabled(command :string,channel: TextChannel){
+        let configChannel = this._config.channels[channel.name+"_"+channel.position];
+       return configChannel && configChannel.modules_enabled[command];
+    }
+}
