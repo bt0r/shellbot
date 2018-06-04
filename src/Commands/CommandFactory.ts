41055ef12d66deb3fccfaa4ad7cf3698421529fe
Thibaut <the.btor@gmail.com>
2018-06-04 21:48:05 +0200

    :hammer: Code formating

--- src/Commands/CommandFactory.ts
+++ src/Commands/CommandFactory.ts
@@ -1,32 +1,30 @@
 'use strict'
 import {Chuck}           from "./Chuck";
 import {Listener}        from "../Listener/Listener";
-import {Message, User}   from "discord.js";
+import {Message}         from "discord.js";
 import {AbstractCommand} from "./AbstractCommand";
 import {Container}       from "typescript-ioc";
 import {Config}          from "../Service/Config";
 
-export class CommandFactory{
-    static instantiate(commandName: string,message: Message){
-        let config = Container.get(Config);
+export class CommandFactory {
+    static instantiate(commandName: string, message: Message) {
+        let config       = Container.get(Config);
         let commandFound = null;
-        switch(commandName){
+        switch (commandName) {
             case Chuck.NAME:
                 commandFound = new Chuck();
                 break;
         }
-        if(commandFound !== null && config.isCommandEnabled(commandName,message.channel)){
-            this.send(commandFound,message);
+        if (commandFound !== null && config.isCommandEnabled(commandName, message.channel)) {
+            this.send(commandFound, message);
         }
     }
-    private static send(command: AbstractCommand,message: Message){
-        command = command.worker(message);
+
+    private static send(command: AbstractCommand, message: Message) {
+        command     = command.worker(message);
         let emitter = Listener.getInstance();
-        emitter.on('shellbot.command',function(data){
-            console.log(data.command.name);
-            console.log(data.command.toto);
-        });
-        emitter.emit("shellbot.command",{
+
+        emitter.emit("shellbot.command", {
             command: command,
             author: message.author
         });
