7ba4a59883e8de523a403870cc9a4a50701715aa
Thibaut <the.btor@gmail.com>
2018-06-10 23:44:10 +0200

    :hammer: Add lastest commands

--- src/Commands/CommandFactory.ts
+++ src/Commands/CommandFactory.ts
@@ -7,6 +7,8 @@ import {Container}       from "typescript-ioc";
 import {Config}          from "../Service/Config";
 import {Boobs}           from "./Boobs";
 import {Qwant}           from "./Qwant";
+import {Cat}             from "./Cat";
+import {Weather}         from "./Weather";
 
 export class CommandFactory {
     static instantiate(commandName: string, message: Message) {
@@ -22,8 +24,19 @@ export class CommandFactory {
             case Qwant.NAME:
                 commandFound = new Qwant();
                 break;
+            case Cat.NAME:
+                commandFound = new Cat();
+                break;
+            case Weather.NAME:
+                commandFound = new Weather();
+                break;
         }
         if (commandFound !== null && config.isCommandEnabled(commandName, message.channel)) {
+            let channel       = message.channel;
+            let configChannel = config.config.channels[channel.name + "_" + channel.position];
+            let commandConfig = configChannel.modules_enabled[commandName];
+            commandFound.config = commandConfig;
+
             this.send(commandFound, message);
         }
     }
